4b66d4ab8f9281fb647850390cfcc3c40cf59e91
Will Greene <117117153+wogreene@users.noreply.github.com>
2025-12-15 20:15:55 -0500

    Create viewer.js

---
+++ js/viewer.js
@@ -0,0 +1,65 @@
+const TILER_BASE =
+  "https://YOUR-TITILER-APP/cog/tiles/{z}/{x}/{y}.png?url=";
+
+const params = new URLSearchParams(window.location.search);
+const reefId = params.get("id");
+
+const map = new maplibregl.Map({
+  container: "map",
+  style: "https://demotiles.maplibre.org/style.json",
+  center: [-77.32, 25.08],
+  zoom: 16
+});
+
+async function init() {
+  const sites = await fetch("data/sites.geojson").then(r => r.json());
+  const feature = sites.features.find(
+    f => f.properties.id === reefId
+  );
+
+  if (!feature) {
+    alert("Reef not found");
+    return;
+  }
+
+  const timepoints = feature.properties.timepoints;
+  const years = Object.keys(timepoints);
+
+  const select = document.getElementById("timepointSelect");
+  years.forEach(y => {
+    const opt = document.createElement("option");
+    opt.value = y;
+    opt.textContent = y;
+    select.appendChild(opt);
+  });
+
+  map.on("load", () => {
+    addRaster(timepoints[years[0]]);
+  });
+
+  select.onchange = () => {
+    switchTimepoint(timepoints[select.value]);
+  };
+}
+
+function addRaster(cogUrl) {
+  map.addSource("reef", {
+    type: "raster",
+    tiles: [TILER_BASE + encodeURIComponent(cogUrl)],
+    tileSize: 256
+  });
+
+  map.addLayer({
+    id: "reef-layer",
+    type: "raster",
+    source: "reef"
+  });
+}
+
+function switchTimepoint(cogUrl) {
+  map.getSource("reef").setTiles([
+    TILER_BASE + encodeURIComponent(cogUrl)
+  ]);
+}
+
+init();
