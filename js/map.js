ab667e6139b3b878ee1db132975366b148f5dcd1
Will Greene <117117153+wogreene@users.noreply.github.com>
2025-12-17 15:15:47 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -95,7 +95,7 @@ map.on("load", async () => {
       "text-size": 14,
       "text-offset": [0, 1.2],
       "text-anchor": "top",
-      "text-font": ["Open Sans Regular"]
+      "text-font": ["Noto Sans Mono Regular"]
     },
     paint: {
       "text-color": "#ffffff",
