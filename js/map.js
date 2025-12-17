427576d5d104854bc08c5de28d35f85daa54d30c
Will Greene <117117153+wogreene@users.noreply.github.com>
2025-12-17 15:09:33 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ const STYLE = {
   version: 8,
 
   // REQUIRED for any text labels
-  glyphs: "https://api.maptiler.com/fonts/{fontstack}/{range}.pbf",
+  glyphs: "https://fonts.openmaptiles.org/{fontstack}/{range}.pbf",
 
   sources: {
     "esri-satellite": {
